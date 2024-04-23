f205b489f621af26de8700d91fe2f6ad34b51901
Lucas Guerra <lucasmazoniguerra@gmail.com>
2024-04-23 14:31:50 -0300

    first project

--- java_project_test/app/src/main/java/java_project_test/App.java
+++ java_project_test/app/src/main/java/java_project_test/App.java
@@ -1,23 +1,37 @@
 package java_project_test;
+
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args){
-    
+
+    public static void main(String[] args) {
+
+        double preco_custo = 0.0f;
+        double preco_venda = 0.0f;
+        String nome_produto = "null";
+        
         Scanner leitor = new Scanner(System.in);
         
-        int idade = leitor.nextInt();
-        float cotacaoDolar = leitor.nextFloat();
-        double cotacaoEuro = leitor.nextDouble();
-        
-        // Consume the newline character left in the buffer
-        leitor.nextLine();
-        
-        String nome = leitor.nextLine();
-        String codigoRua = leitor.next();
-        
-        System.out.println("texto que sera exibido");
-        System.out.print("texto que sera exibido");
-        
+        for(int i = 1; i <= 40; ++i){
+            
+            System.out.println("ÇDigite o nome do produto: ");
+            nome_produto = leitor.next();
+            
+            System.out.println("Digite o preco do produto: ");
+            preco_custo = leitor.nextDouble();
+            
+            System.out.println("Digite o preco de venda: ");
+            preco_custo = leitor.nextDouble();
+            
+            double diferenca_valor = preco_venda - preco_custo;
+            
+            if(diferenca_valor > 0){
+                System.out.println("Valor positivo");
+            } else if (diferenca_valor < 0) {
+                System.out.println("Valor negativo");
+            }else{
+                System.out.println("empate entre custo e venda");
+            }                             
+        }   
     }
 }
